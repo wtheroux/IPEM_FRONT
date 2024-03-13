a6b976031f038457b906390a635e6ba6cb9df691
marat <mahmutov@nvtech.ru>
2024-03-13 17:26:18 +0500

    #3 test husky

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:prettier/recommended",
+    ],
+    ignorePatterns: ["build", ".eslintrc.js", 'node_modules', 'prettier.config.js'],
+    parser: "@typescript-eslint/parser",
+    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+    plugins: ["react-hooks", "react", "prettier"],
+    rules: {
+      "prettier/prettier": ["error"],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      "react/self-closing-comp": ["error", {
+        "component": true,
+        "html": true
+      }],
+      "arrow-body-style": ["error", "as-needed"],
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  }
